d19c2d705e4d85a5edf0d0058bac3783efd4cd84
Karim <karim.wael@gmail.com>
2022-01-20 09:43:53 +0400

    Basic Gin Setup

--- main.go
+++ main.go
@@ -1,9 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Identity struct {
+	Email     string `json:"email"`
+	UserID    string `json:"user_id"`
+	SessionId string `json:"session_id"`
+}
 
 func main() {
-	timeNow := getTimeInISOString()
-	fmt.Println(timeNow)
-	fmt.Println(get30SecondsFromTime(timeNow))
+
+	r := gin.Default()
+	r.POST("/test", handleTest)
+	r.Run()
+}
+func handleTest(c *gin.Context) {
+	fmt.Println(c.PostForm("Grant_Type"))
+	// fmt.Println(c.PostFormArray("Payload"))
+	var ids []Identity
+	json.Unmarshal([]byte(c.PostForm("Payload")), &ids)
+	fmt.Println(ids[0].UserID)
+	c.JSON(200, gin.H{
+		"payload": ids,
+	})
 }
