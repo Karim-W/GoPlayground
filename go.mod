499c4a94490a67f695064eef81d8a9da2331cc54
Karim <karim.wael@gmail.com>
2022-04-07 19:31:14 +0400

    sigterm handler

--- go.mod
+++ go.mod
@@ -1,5 +1,26 @@
 module GoPlayground
 
-go 1.17
+go 1.18
 
-require golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce
+require (
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.13.0 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/ugorji/go/codec v1.1.7 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
+
+require (
+	github.com/gin-gonic/gin v1.7.7
+	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,82 @@
 package main
 
+import (
+	"log"
+	"os"
+	"os/signal"
+	"reflect"
+	"syscall"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	Rout()
+
+	go func() {
+		// start your software here. Maybe your need to replace the for loop with other code
+		r := gin.Default()
+		r.GET("/ping", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "pong",
+			})
+		})
+		r.Run()
+	}()
+	c1 := CreateCallabeFunction(test)
+	c2 := CreateCallabeFunction(intTwo, 2)
+	HandleExits(c1, c2)
+}
+
+func test() {
+	log.Println("i wanna die")
+}
+func intTwo(a int) int {
+	log.Println(a * 2)
+	return a * 2
+}
+
+type CallableFunction struct {
+	fun  any
+	args []reflect.Value
+}
+
+func CreateCallabeFunction(fun any, args ...any) CallableFunction {
+	functionArguments := make([]reflect.Value, len(args))
+	for i := range args {
+		functionArguments[i] = reflect.ValueOf(args[i])
+	}
+	return CallableFunction{
+		fun:  fun,
+		args: functionArguments,
+	}
+}
+
+func HandleExits(args ...CallableFunction) {
+	cancelChan := make(chan os.Signal, 1)
+	signal.Notify(cancelChan, syscall.SIGTERM, syscall.SIGINT)
+	sig := <-cancelChan
+	log.Printf("Caught SIGTERM %v", sig)
+	log.Println("Exiting...")
+	for i := range args {
+		fc := args[i]
+		v := reflect.ValueOf(fc.fun)
+		if v.Kind() != reflect.Func {
+			panic("argument must be a function")
+		} else {
+			v.Call(fc.args)
+		}
+
+	}
+
+	//Something like this?
+	//workyy
+	//Need to clean up though but yay
+	//yup yup
+	//i go back to pachirisu/zogor now. bye
+	// lettuce see
+	//
+	// for i := 0; i < 10; i++ {
+	// 	log.Println("i wanna die")
+	// }
+
 }
