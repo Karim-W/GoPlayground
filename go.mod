46736ff3eac09928bb16cf0bfcd5d688b2580214
Karim <karim.wael@gmail.com>
2022-09-10 06:32:02 +0400

    watermill

--- go.mod
+++ go.mod
@@ -2,4 +2,22 @@ module GoPlayground
 
 go 1.17
 
-require golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce
+require (
+	github.com/ThreeDotsLabs/watermill v1.2.0-rc.7
+	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce
+)
+
+require (
+	github.com/cenkalti/backoff/v3 v3.2.2 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rabbitmq/amqp091-go v1.2.0 // indirect
+)
+
+require (
+	github.com/ThreeDotsLabs/watermill-amqp/v2 v2.0.6
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/lithammer/shortuuid/v3 v3.0.7 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,65 @@
 package main
 
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-amqp/v2/pkg/amqp"
+	"github.com/ThreeDotsLabs/watermill/message"
+)
+
+var amqpURI = "amqp://guest:guest@localhost:5672/"
+
 func main() {
-	Rout()
+	amqpConfig := amqp.NewDurableQueueConfig(amqpURI)
+
+	subscriber, err := amqp.NewSubscriber(
+		// This config is based on this example: https://www.rabbitmq.com/tutorials/tutorial-two-go.html
+		// It works as a simple queue.
+		//
+		// If you want to implement a Pub/Sub style service instead, check
+		// https://watermill.io/pubsubs/amqp/#amqp-consumer-groups
+		amqpConfig,
+		watermill.NewStdLogger(false, false),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	messages, err := subscriber.Subscribe(context.Background(), "example.topic")
+	if err != nil {
+		panic(err)
+	}
+
+	go process(messages)
+	publisher, err := amqp.NewPublisher(amqpConfig, watermill.NewStdLogger(false, false))
+	if err != nil {
+		panic(err)
+	}
+
+	go publishMessages(publisher)
+	select {}
+}
+
+func process(messages <-chan *message.Message) {
+	for msg := range messages {
+		log.Printf("received message: %s, payload: %s", msg.UUID, string(msg.Payload))
+
+		// we need to Acknowledge that we received and processed the message,
+		// otherwise, it will be resent over and over again.
+		msg.Ack()
+	}
+}
+func publishMessages(publisher message.Publisher) {
+	for {
+		msg := message.NewMessage(watermill.NewUUID(), []byte("Hello, world!"))
+
+		if err := publisher.Publish("example.topic", msg); err != nil {
+			panic(err)
+		}
+
+		time.Sleep(time.Second)
+	}
 }
