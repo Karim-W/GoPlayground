0ff57f37cb3f57e1eadd2077bd95a2542510ed9a
Karim-W <karim.wael@gmail.com>
2022-12-23 11:15:28 +0400

    played with RXGo

--- go.mod
+++ go.mod
@@ -2,4 +2,15 @@ module GoPlayground
 
 go 1.17
 
-require golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce
+require github.com/reactivex/rxgo/v2 v2.5.0
+
+require (
+	github.com/cenkalti/backoff/v4 v4.0.0 // indirect
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/emirpasic/gods v1.12.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.0 // indirect
+	github.com/stretchr/testify v1.4.0 // indirect
+	github.com/teivah/onecontext v0.0.0-20200513185103-40f981bfd775 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,114 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+
+	rxgo "github.com/reactivex/rxgo/v2"
+)
+
+type Customer struct {
+	ID, Age                   int
+	Name, LastName, TaxNumber string
+}
+
 func main() {
-	Rout()
+	// Create an observable from a channel
+	ch := make(chan rxgo.Item)
+	go Producer(ch)
+	observable := rxgo.FromChannel(ch)
+	observable.ForEach(func(v interface{}) {
+		fmt.Printf("received: %v\n", v)
+	}, func(err error) {
+		fmt.Printf("error: %e\n", err)
+	}, func() {
+		fmt.Println("observable is closed")
+	})
+	time.Sleep(1 * time.Minute)
+	//
+}
+
+func Producer(ch chan rxgo.Item) {
+	ch <- rxgo.Item{
+		V: "Hello",
+	}
+	time.Sleep(1 * time.Second)
+	ch <- rxgo.Item{
+		V: "everyone",
+	}
+	time.Sleep(1 * time.Second)
+	ch <- rxgo.Item{
+		V: "!",
+	}
+	time.Sleep(1 * time.Second)
+	ch <- rxgo.Item{
+		V: "I'm",
+	}
+	time.Sleep(1 * time.Second)
+	ch <- rxgo.Item{
+		V: "a",
+	}
+	time.Sleep(1 * time.Second)
+	ch <- rxgo.Item{
+		V: "stream",
+	}
+	time.Sleep(1 * time.Second)
+	ch <- rxgo.Item{
+		V: "of",
+	}
+	time.Sleep(1 * time.Second)
+	ch <- rxgo.Item{
+		V: "strings",
+	}
+	///////////////////////////
+	customerChan := make(chan rxgo.Item)
+	go CustomerProducer(customerChan)
+
+	observable := rxgo.FromChannel(customerChan)
+	observable.Filter(func(v interface{}) bool {
+		customer := v.(Customer)
+		return customer.Age > 18
+	}).Map(func(_ context.Context, item interface{}) (interface{}, error) {
+		// Enrich operation
+		customer := item.(Customer)
+		fmt.Println("Assginning tax number to customer")
+		customer.TaxNumber = RandomlyGenerateString()
+		return customer, nil
+	}).ForEach(func(v interface{}) {
+		fmt.Printf("received: %v\n", v)
+	}, func(err error) {
+		fmt.Printf("error: %e\n", err)
+	}, func() {
+		fmt.Println("observable is closed")
+	})
+	time.Sleep(1 * time.Minute)
+	///////////////////////////
+
+}
+
+func RandomlyGenerateString() string {
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(100)
+	return fmt.Sprintf("TN%d", n)
+}
+
+func CustomerProducer(
+	ch chan rxgo.Item,
+) {
+	for i := 0; i < 10; i++ {
+		rand.Seed(time.Now().UnixNano())
+		age := rand.Intn(100)
+		customer := Customer{
+			ID:       i,
+			Age:      age,
+			Name:     fmt.Sprintf("Name %d", i),
+			LastName: fmt.Sprintf("LastName %d", i),
+		}
+		ch <- rxgo.Item{
+			V: customer,
+		}
+		time.Sleep(1 * time.Second)
+	}
 }
